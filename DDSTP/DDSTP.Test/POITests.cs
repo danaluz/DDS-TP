89d62578bacf8b2efc3664a880b5cffbc716569b
Dana <dana.luz@hotmail.com.ar>
2016-04-05 00:45:02 -0300

    Cambio en la ubicación del método de comparacion.

--- DDSTP/DDSTP.Test/POITests.cs
+++ DDSTP/DDSTP.Test/POITests.cs
@@ -11,16 +11,55 @@ namespace DDSTP.Test
         public void CompareTwoPOIs()
         {
             var poi1 = new POI();
-            poi1.Latitude = -34.580442f;
-            poi1.Longitude = -58.411705f;
+            poi1.Latitude = -34.581828f;
+            poi1.Longitude = -58.412723f;
 
             var poi2 = new POI();
-            poi2.Latitude = -34.580358f;
-            poi2.Longitude = -58.411973f;
+            poi2.Latitude = -34.581475f;
+            poi2.Longitude = -58.420244f;
 
-            var result = poi1.IsNearFrom(30, poi2);
+            var result = poi1.IsNearFrom(150, poi2);
             Assert.IsTrue(result);
            
         }
+
+        [TestMethod]
+        public void DistanceBetweenTwoPointsTest()
+        {
+            var poi1 = new POI();
+            poi1.Latitude = -34.581828f;
+            poi1.Longitude = -58.412723f;
+
+            var poi2 = new POI();
+            poi2.Latitude = -34.581475f;
+            poi2.Longitude = -58.420244f;
+
+            
+            var result = DistanceBetweenTwoPoints.distance(poi1.Latitude, poi1.Longitude, poi2.Latitude, poi2.Longitude);
+
+            Assert.IsTrue(result < 700);
+
+        }
+
+        [TestMethod]
+        public void IsNearFromTest()
+        {
+            var poi1 = new POI();
+            poi1.Latitude = -34.581828f;
+            poi1.Longitude = -58.412723f;
+
+            var poi2 = new POI();
+            poi2.Latitude = -34.581475f;
+            poi2.Longitude = -58.420244f;
+
+            var result = poi1.IsNearFrom(600, poi2); 
+
+            Assert.IsTrue(result);
+
+        }
+
+
+
+
     }
 }
