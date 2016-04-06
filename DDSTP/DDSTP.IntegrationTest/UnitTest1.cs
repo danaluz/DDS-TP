3dca7d851a763e1c831247ffeb8e081f347cf10a
Dana <dana.luz@hotmail.com.ar>
2016-04-06 16:03:56 -0300

    Configuración autorestore de paquetes NuGet

---
+++ DDSTP/DDSTP.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DDSTP.Data;
+using DDSTP.Domain;
+
+namespace DDSTP.IntegrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dbContext = new dbDDSTPContext();
+            var poi1 = new POI();
+            poi1.Latitude = -34.581828f;
+            poi1.Longitude = -58.412723f;
+            poi1.Name="Prueba1";
+            poi1.Number=15;
+            poi1.Type= TypeOfPOI.Bank;
+
+            dbContext.POIs.Add(poi1);
+            dbContext.SaveChanges();
+
+        }
+    }
+}
